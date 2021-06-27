888f7382231d6dd9bb8586b6828ba7754e40c4c9
Rehane <abusayid693@gmail.com>
2021-06-27 22:58:04 +0530

    email added

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,8 +5,8 @@ import "./footer.scss"
 export default function Footer(){
     return(
         <div className="footer">
-        <a href="https://discord.gg/un2USMtv"><i class="fab fa-3x fa-discord"></i></a>
-        <a href="https://limbo-hacks-12968.devpost.com/"><i class="fab fa-3x fa-dev"></i></a>
+        <a href="https://discord.gg/un2USMtv" target="_blank"><i class="fab fa-3x fa-discord"></i></a>
+        <a href="https://limbo-hacks-12968.devpost.com/" target="_blank"><i class="fab fa-3x fa-dev"></i></a>
       <p> Made with ❤️ by Team Limbo Hacks</p>
         </div>
     );
