ff684d1cc410ae0dc3adef09f687a31babf063f7
Zoheb Alli Khan <understandingzak@gmail.com>
2021-07-02 21:18:31 +0530

    Added Twitter Handle and fixed some typos

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -7,40 +7,62 @@ import Mail from "./icons8-mail.svg";
 import DevPost from "./icons8-dev-post.svg";
 import PrivacyPolicy from "./Privacy policy.pdf";
 import TermsOfUse from "./Terms of use.pdf";
+import Twitter from "./icons8-twitter.svg";
 export default function Footer() {
   return (
     <div>
       <div className="footer">
         <div className="social-icons-container">
           <div className="social-icon">
-            <a rel="noreferrer" target="_blank" href="">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.instagram.com/limbohacks/"
+            >
               <img src={Insta} alt="" />
             </a>
           </div>
           <div className="social-icon">
-            <a rel="https://discord.gg/un2USMtv" target="_blank" href="">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://discord.gg/un2USMtv"
+            >
               <img src={Dis} alt="" />
             </a>
           </div>
           <div className="social-icon">
             <a
-              rel="https://www.linkedin.com/company/limbo-hacks/"
+              rel="noreferrer"
               target="_blank"
-              href=""
+              href="https://www.linkedin.com/company/limbo-hacks/"
             >
               <img src={Linked} alt="" />
             </a>
           </div>
           <div className="social-icon">
-            <a rel="noreferrer" target="_blank" href="">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="mailto:hello@limbohacks.tech"
+            >
               <img src={Mail} alt="" />
             </a>
           </div>
           <div className="social-icon">
             <a
-              rel="https://limbo-hacks-12968.devpost.com/"
+              rel="noreferrer"
               target="_blank"
-              href=""
+              href="https://twitter.com/HacksLimbo"
+            >
+              <img src={Twitter} alt="" />
+            </a>
+          </div>
+          <div className="social-icon">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://limbo-hacks-12968.devpost.com/"
             >
               <img src={DevPost} alt="" />
             </a>
