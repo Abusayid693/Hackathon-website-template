773089ff13487e99cafa892b7b40a04ff69f9f31
Zoheb Alli Khan <understandingzak@gmail.com>
2021-07-29 20:18:58 +0530

    Routes

---
+++ src/components/Routes/Instagram.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+function Instagram() {
+  useEffect(() => {
+    window.location.href = "https://www.instagram.com/limbohacks/";
+  }, []);
+  return <div></div>;
+}
+
+export default Instagram;
