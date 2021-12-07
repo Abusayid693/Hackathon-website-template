ef43e69a325e878dc48dab24635932b08dddf4b9
Rehan Choudhury <Abusayid693@gmail.com>
2021-12-08 00:38:00 +0530

    Just a test PR

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import {Link} from 'gatsby';
 // styles
 const pageStyles = {
   color: '#232129',
-  padding: '99px',
+  padding: '90px',
   fontFamily: '-apple-system, Roboto, sans-serif, serif',
 };
 const headingStyles = {
