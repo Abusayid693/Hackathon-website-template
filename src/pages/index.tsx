126fec42d20b63649a370ae2bcf6dc44c6d5cae3
Rehan Choudhury <Abusayid693@gmail.com>
2021-12-07 19:05:50 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,35 +84,35 @@ const links = [
     text: 'Tutorial',
     url: 'https://www.gatsbyjs.com/docs/tutorial/',
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+      "A great place ",
     color: '#E95800',
   },
   {
     text: 'How to Guides',
     url: 'https://www.gatsbyjs.com/docs/how-to/',
     description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+      "Practical step-by-s",
     color: '#1099A8',
   },
   {
     text: 'Reference Guides',
     url: 'https://www.gatsbyjs.com/docs/reference/',
     description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+      "Nitty-gritty technical ",
     color: '#BC027F',
   },
   {
     text: 'Conceptual Guides',
     url: 'https://www.gatsbyjs.com/docs/conceptual/',
     description:
-      'Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.',
+      'Big-picture explanations ',
     color: '#0D96F2',
   },
   {
     text: 'Plugin Library',
     url: 'https://www.gatsbyjs.com/plugins',
     description:
-      'Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.',
+      'Add functionality and cu',
     color: '#8EB814',
   },
   {
@@ -120,7 +120,7 @@ const links = [
     url: 'https://www.gatsbyjs.com/cloud',
     badge: true,
     description:
-      'Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!',
+      'Now you’re ready to show the ',
     color: '#663399',
   },
 ];
@@ -133,7 +133,6 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         Congratulations
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
@@ -149,7 +148,7 @@ const IndexPage = () => {
         <li style={docLinkStyle}>
           <a
             style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+   
           >
             {docLink.text}
           </a>
@@ -159,7 +158,6 @@ const IndexPage = () => {
             <span>
               <a
                 style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
               >
                 {link.text}
               </a>
@@ -175,7 +173,6 @@ const IndexPage = () => {
       </ul>
       <img
         alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
     </main>
   );
