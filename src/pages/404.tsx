8c5f45b6432dd7cfea5aab400ea58120f11759df
Rehan Choudhury <Abusayid693@gmail.com>
2021-12-07 20:04:35 +0530

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import {Link} from 'gatsby';
 // styles
 const pageStyles = {
   color: '#232129',
-  padding: '160px',
+  padding: '16px',
   fontFamily: '-apple-system, Roboto, sans-serif',
 };
 const headingStyles = {
