b1adabef4b4a0f0b1886aad4a81e44c2c4f72485
Rehane <abusayid693@gmail.com>
2021-12-07 16:42:56 +0530

    actions testing

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import {Link} from 'gatsby';
 // styles
 const pageStyles = {
   color: '#232129',
-  padding: '96px',
+  padding: '99px',
   fontFamily: '-apple-system, Roboto, sans-serif, serif',
 };
 const headingStyles = {
