f5fbe9a3e26152ed9f8b033230bca4da6938f61a
Rehane <abusayid693@gmail.com>
2021-12-11 23:57:06 +0530

    Calender reducer added

--- src/components/Calender/index.tsx
+++ src/components/Calender/index.tsx
@@ -15,6 +15,7 @@ interface eventType {
 
 interface dataProps {
   day: number;
+  dumpDay?:boolean;
   event: boolean;
   events: eventType[];
 }
@@ -118,11 +119,95 @@ const data: dataProps[] = [
   },
 ];
 
-export const Calender = () => {
-  const [index, setIndex] = useState(1);
-  const [selectedDate, setSelectedDate] = useState(0);
+interface payloadTypes {
+  type: string;
+  data?: number | string | dataProps[];
+}
 
-  if (index === 1) {
+
+
+
+
+
+
+export const Calender = () => {
+  const initialState = {
+    index: 1,
+    selectedData: 0,
+    month: 1,
+    year: 2021,
+    dates: data,
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const calenderReducer = (state = initialState, payload: payloadTypes) => {
+    switch (payload.type) {
+      case 'UPDATE_INDEX':
+        // @ts-ignore
+        setState(state => ({...state, index: payload.data}));
+        break;
+      case 'SHOW_EVENTS_FOR_SELECTED_DATE':
+        // @ts-ignore
+        setState(state => ({...state, selectedData: payload.data}));
+        break; 
+      case 'SWITCH_MONTHS':
+        // @ts-ignore
+        setState(state => ({...state, dates: payload.data}));
+        break;  
+    }
+  };
+
+
+
+  useEffect(()=>{
+
+    let num = 28;
+
+    let structureToHoldDates:dataProps[] = [];
+
+
+    for (let i = 0; i<= num+4; i++){
+       if(i>4){
+        structureToHoldDates.push({
+          day: i,
+          event: true,
+          dumpDay:false,
+          events: [
+            {
+              title: 'Intro to python',
+              timings: '12 AM - 1 Pm',
+              content: 'Intro to python .....',
+              ended: false,
+            },
+            {
+              title: 'Intro to Java',
+              timings: '12 AM - 1 Pm',
+              content: 'Intro to python .....',
+              ended: false,
+            },
+          ],
+        })
+       }
+       else {
+        structureToHoldDates.push(
+          {
+            day: i,
+            event: false,
+            dumpDay:true,
+            events: [],
+        })
+       }
+    }
+
+    calenderReducer(state,{type:'SWITCH_MONTHS',data:structureToHoldDates})
+ 
+  },[])
+
+
+
+
+  if (state.index === 1) {
     return (
       <H.Container>
         <Flexbox className="card-top" flexRow>
@@ -145,14 +230,16 @@ export const Calender = () => {
             </Flexbox>
           ))}
 
-          {data.map((day, j) => (
+          {state.dates.map((day, j) => (
             <Flexbox
               alignCenter
               justifyCenter
               className="card-body-inner"
               onClick={() => {
-                setIndex(2);
-                setSelectedDate(j);
+                // setIndex(2);
+                calenderReducer(state,{type:'UPDATE_INDEX',data:2})
+                // setSelectedDate(j);
+                calenderReducer(state,{type:'SHOW_EVENTS_FOR_SELECTED_DATE',data:j})
               }}
             >
               <p>{day.day}</p>
@@ -161,11 +248,11 @@ export const Calender = () => {
         </div>
       </H.Container>
     );
-  } else if (index === 2) {
+  } else if (state.index === 2) {
     return (
       <H.Container>
-        {data[selectedDate].event ? (
-          data[selectedDate].events.map((event, j) => (
+        {state.dates[state.selectedData].event ? (
+          state.dates[state.selectedData].events.map((event, j) => (
             <H.Event>
               <h3>{event.title}</h3>{' '}
             </H.Event>
