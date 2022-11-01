48271e366a3ac0294e68c9ee87cd9a3095516c89
Muky-dev <ti.gabrielfini@gmail.com>
2022-11-01 13:13:32 -0300

    Margin on text

--- src/components/LowCode/index.jsx
+++ src/components/LowCode/index.jsx
@@ -1,22 +1,22 @@
 import {LOW_CODE_SECTION} from 'Module/General';
-import {Row} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
 
 const LowCode = () => {
   return (
     <Row className="lowCodeSection">
-      <div className="title_wrapper">
+      <Col className="title_wrapper">
         <h1 className="title_default">{LOW_CODE_SECTION.TITLE}</h1>
-      </div>
-      <div className="imgContainer">
+      </Col>
+      <Col className="imgContainer">
         <img
           className="Banner img-rounded"
           src={LOW_CODE_SECTION.SRC}
           alt="lowCode"
         />
-      </div>
-      <div>
+      </Col>
+      <Col>
         <p className="lowCodeDescription">{LOW_CODE_SECTION.DESCRIPTION}</p>
-      </div>
+      </Col>
     </Row>
   );
 };
