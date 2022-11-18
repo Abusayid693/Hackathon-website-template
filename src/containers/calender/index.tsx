cc3b10a630483dfb9c8c88ddf44e986a045b54a9
olsenbudanur <olsenbudanur@gmail.com>
2022-11-18 18:01:30 -0500

    Logo

--- src/containers/calender/index.tsx
+++ src/containers/calender/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import {schedule} from '../../Module/General';
 import {Calender} from './calender.component';
 
 export default function MyCalender(props: any) {
-  return <Calender data={schedule} {...props} />;
+  return <Calender />;
 }
