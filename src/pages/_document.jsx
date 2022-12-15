c1d5b0b9ae43dee242325ef07f2f7168a8ab4936
Muky-dev <ti.gabrielfini@gmail.com>
2022-12-15 13:22:35 -0300

    Added analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,5 @@
 import {Head, Html, Main, NextScript} from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -17,6 +18,21 @@ export default function Document() {
           src="https://kit.fontawesome.com/fc6a27a00b.js"
           crossOrigin="anonymous"
         ></script>
+        {/*<!-- Google tag (gtag.js) -->*/}
+        <Script
+          async
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-HTWRB8Q6KD"
+        />
+        <Script id="analytics" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HTWRB8Q6KD');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
